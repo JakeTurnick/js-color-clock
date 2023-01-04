3336d043e3a3105dad457f430a375048f15e7e46
JakeTurnick <jaketurnick@gmail.com>
2023-01-04 14:12:21 -0500

    % to next minute functional

--- main.js
+++ main.js
@@ -18,15 +18,18 @@ function tellTime() {
     //Adds 0's to pad clock (ie: 10:09 instead of 10:9)
     if (sec < 10) {
         sec = "0" + sec;
-    }
+    };
     if (min < 10) {
         min = "0" + min;
-    }
+    };
     if (hrs < 10) {
         hrs = "0" + hrs;
-    }
+    };
     //log time to console & change HTML to display time
     console.log(`${hrs}:${min}:${sec}`);
     clock[0].innerHTML = (`${hrs}:${min}:${sec}`);
-}
+    const tFactor = 100/60;
+    let percent = sec * tFactor;
+    console.log(`${percent.toFixed(2)}% to next minute`)
 
+}
