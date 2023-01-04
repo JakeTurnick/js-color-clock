b4eaf36f6e67ba276a616f6f892b9ae12d31a696
JakeTurnick <jaketurnick@gmail.com>
2023-01-04 14:07:32 -0500

    added comments

--- main.js
+++ main.js
@@ -10,16 +10,22 @@ setInterval(tellTime, 1000);
 
 //Pulls new date and gets time
 function tellTime() {
+    //get new date, set variables for HH:MM:SS
     let now = new Date;
     let hrs = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
+    //Adds 0's to pad clock (ie: 10:09 instead of 10:9)
     if (sec < 10) {
         sec = "0" + sec;
     }
     if (min < 10) {
         min = "0" + min;
     }
+    if (hrs < 10) {
+        hrs = "0" + hrs;
+    }
+    //log time to console & change HTML to display time
     console.log(`${hrs}:${min}:${sec}`);
     clock[0].innerHTML = (`${hrs}:${min}:${sec}`);
 }
